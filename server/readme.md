4eb96c0a9d7b71871f8446e2e5518ff142ca708a
Walany Costa <walanybnegro@gmail.com>
2023-02-03 12:48:56 +0100

    feat[server]: add route games method GET

---
+++ server/readme.md
@@ -0,0 +1,28 @@
+## Entidades
+
+### Game
+id
+title
+bannerUrl
+
+
+
+### Ad
+
+id
+gameId
+name
+yearsPlaying
+discord
+weekDays
+hourStart
+hourEnd
+UseVoiceChannel
+createdAt
+
+## casos de uso
+
+- Listagem de games com contagem de anúncios
+- criação de novo anúncio
+- Listagem de anúncios por game
+- Buscar discord pelo ID do anúncio
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,24 @@
- import express from 'express';
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
+const prisma = new PrismaClient({
+    log: ['query']
+});
 
-app.get('/ads', (request, response) => {
-    return response.json([
-        {id: 1, name: "Anuncio 1"},
-        {id: 2, name: "Anuncio 2"},
-        {id: 3, name: "Anuncio 3"},
-        {id: 4, name: "Anuncio 4"},
-    ])
-})
+app.get('/games', async (request, response) =>{
+    const games = await prisma.game.findMany({
+        include: {
+            _count:{
+                select:{
+                    ads: true,
+                }
+            }
+        }
+    });
+    return response.json(games);
+});
+
 
 app.listen(3333, ()=>{
     console.log('Servidor rodando...')
